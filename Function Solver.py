265e05c22ed27494631ecffa4e22fc49e032c3c0
Connor <50525187+WobblyRonnoc@users.noreply.github.com>
2022-05-02 11:36:26 -0400

    Restructured functions, comments, feature complete

--- Function Solver.py
+++ Function Solver.py
@@ -1,74 +1,63 @@
-from simpleeval import simple_eval 
-import parser
+# list of values of x
+xValues = []
 
+# Prompts the user for a minimum and maximum value, 
+# returns a list of values within given range
+def getRange():
+    while(True):
+        try:
+            minimum = int(input("Enter a minimum x value: "))
+            maximum = int(input("Enter a maximum x value: "))
+        except: 
+            print("Error: Not a valid integer.")
+        break
+    for n in range(minimum, (maximum)+1):
+        xValues.append(n)
+    return xValues
 
-#?  a list of values of x
-xValues = [] 
+# Prompts the user for a number of x values then, what each value is
+# Returns the list of values
+def getManual():
+    try:
+        howMany = int(input("How many x values?: "))
+        for i in range(0, howMany):
+            nval = int(input("Enter an x value: "))
+            xValues.append(nval)
+    except:
+        print("oops... \n\n\n\n")
+    return xValues
 
-#! CLEAR THE xValues LIST AND USE IT FOR MANUAL AND RANGE USER OPTIONS 
-#! THIS IS REDUNDANT 
-#? init for looping
-vals2 = [] 
-
-def doRng(minimum,maximum):
-    #*ENTER RANGE (MIN,MAX+1)
-    for n in range(minimum,maximum):
-        vals2.append(n)
-    return vals2
-
-def f(x):
-    return (-2*(x**2)-5) #?<-----enter question here
-
-# prompt the user for the expression 
-# returns valid expression  
+# Prompt the user for the expression
+# Returns valid expression
 def getExpression():
     while(True):
         try:
             exp = str(input("Enter an equation to solve for a single variable (x):\n> "))
             break
         except:
-            print("oops... Try again \n")
+            print("oops... Try something like this: '(2 + 4) * x - 8' \n")
     return exp
 
-def generateOrderedPairs(v=xValues):
-    for n in v:
-        print(f"({n}, {expression})") #! EXPRESSION WITH x SUBBED FOR AS THE y COORDINATE
-        
-
-def mathHelper9000():
+# Prompt user for type of coordinate generation
+# runs the function corresponding to the choice 
+# returns the x values list
+def getXValues():
     asking = True
     while (asking):
         try:
-            ans = str(input("\nMANUAL x values OR RANGE of x:\n> "))
+            print("Enter 'manual' for specific x values \nOR\nEnter 'range' for a range of x values")
+            ans = str(input("> "))
             if (ans == "manual"):
-                try:
-                    howMany = int(input("How many x values?: "))
-                    for i in range(0,howMany):
-                        nval = int(input("Enter an x value: "))
-                        xValues.append(nval)
-                except: 
-                    print("oops... \n\n\n\n")
-                print("\n")    
-                generateOrderedPairs(xValues) 
-                print("\n")
-            elif (ans == "range"):
-                try:
-                    minimum = int(input("Enter a minimum x value: "))
-                    maximum = int(input("Enter a MAXIMUM x value: "))
-                except: 
-                    print("oops... \n\n\n\n")
-                    
-                doRng(minimum,maximum)
-                print("\n")    
-                generateOrderedPairs(vals2) 
-                print("\n")
-            else:
-                print("Exiting program...")
-                asking = False
+                return getManual()
+            elif(ans == "range"):
+                return getRange()
         except:
-            print("oops... Try again\n")
+            print("error: not a valid choice. - your options are 'manual' or 'range'.")
             
-    print("\n"*5)
-    
 expression = getExpression()
-mathHelper9000()
+x = getXValues()
+
+# generate the ordered pairs
+for x in xValues:
+    y = eval(expression)
+    print(f"({x}, {y})")
